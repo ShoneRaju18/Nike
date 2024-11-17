de735a99a9859f7138fb81a5c98af82a31c7f9ba
Shone Raju <shoneraju@Shones-Mac-mini.local>
2024-11-17 23:38:30 +0530

    Popular Products Section Completed

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -8,6 +8,8 @@ const PopularProductCard = ({imgURL, name, price}) => {
             <img src={star} alt="rating" width={24} height={24}/>
             <p className="text-xl leasing-normal text-slate-gray font-montserrat">(4.5)</p>
         </div>
+        <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">{name}</h3>
+        <p className="mt-2 font-semibold font-montserrat text-coral-red text-2xl leading-normal">{price}</p>
     </div>
   )
 }
